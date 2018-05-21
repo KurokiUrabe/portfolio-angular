d3720a92adf652a1c196436d0b68601a23e1cc99
Hello MH <arkangel.exe@gmail.com>
2018-05-21 18:36:58 -0500

    Update with apollo
    HttpLinkModule It’s optional, you can replace it with any other Link. Its biggest advantage of all is that it uses HttpClient internally so it’s possible to use it in NativeScript or in combination with any other HttpClient provider. By using HttpLinkModule you get Server-Side Rendering for free, without any additional work.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,20 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import { BrowserModule } from "@angular/platform-browser";
+import { NgModule } from "@angular/core";
+import { HttpClientModule } from "@angular/common/http";
+import { ApolloModule } from "apollo-angular";
+import { HttpLinkModule } from "apollo-angular-link-http";
 
-import { AppComponent } from './app.component';
-
+import { AppComponent } from "./app.component";
+@NgModule({})
 @NgModule({
-  declarations: [
-    AppComponent
-  ],
+  declarations: [AppComponent],
   imports: [
-    BrowserModule
+    BrowserModule,
+    HttpClientModule, // provides HttpClient for HttpLink
+    HttpLinkModule,
+    ApolloModule
   ],
   providers: [],
   bootstrap: [AppComponent]
 })
-export class AppModule { }
+export class AppModule {}
