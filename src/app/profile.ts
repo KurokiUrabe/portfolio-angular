b005830d2f9860a9d154acecfc7bf4549679982e
Hello MH <arkangel.exe@gmail.com>
2018-05-27 19:22:48 -0500

    class Profile
    basic data for my profile

---
+++ src/app/profile.ts
@@ -0,0 +1,11 @@
+export class Profile {
+  picture: string;
+  name: string;
+  nickname: string;
+  bio: string;
+  sumary: string;
+  email: string;
+  city: string;
+  state: string;
+  portfolio: string;
+}
