d2b50e4b0c2d94f51276fab1a6351164cab63850
Hello MH <arkangel.exe@gmail.com>
2018-05-27 19:23:16 -0500

    detail component

---
+++ src/app/employer-detail/employer-detail.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit, Input } from "@angular/core";
+import { Employer, Query } from "../types";
+
+@Component({
+  selector: "app-employer-detail",
+  templateUrl: "./employer-detail.component.html",
+  styleUrls: ["./employer-detail.component.css"]
+})
+export class EmployerDetailComponent implements OnInit {
+  @Input() employer: Employer;
+
+  constructor() {}
+  ngOnInit() {}
+}
