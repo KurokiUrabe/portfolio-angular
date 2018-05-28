50e7e6f9e31c0b3d5275c10ddfc9227108faf2a1
Hello MH <arkangel.exe@gmail.com>
2018-05-27 19:22:24 -0500

    mock hitman for test

---
+++ src/app/mocks.ts
@@ -0,0 +1,19 @@
+import { Employer } from "./types";
+
+export const EMPLOYER: Employer[] = [
+  {
+    jobTitle: "killer",
+    company: "hut",
+    description: "poorly"
+  },
+  {
+    jobTitle: "hitman",
+    company: "den",
+    description: "crazy"
+  },
+  {
+    jobTitle: "sniper",
+    company: "nest",
+    description: "grate"
+  }
+];
