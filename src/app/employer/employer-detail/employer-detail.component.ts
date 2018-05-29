da7094141f56f3541972e999cf8a8ec310097225
Hello MH <arkangel.exe@gmail.com>
2018-05-28 23:49:36 -0500

    detail of employer, simple ez

--- src/app/employer/employer-detail/employer-detail.component.ts
+++ src/app/employer/employer-detail/employer-detail.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit, Input } from "@angular/core";
-import { Employer, Query } from "../types";
+import { Employer, Query } from "../../types";
 
 @Component({
   selector: "app-employer-detail",
